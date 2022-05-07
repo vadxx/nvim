b87ec83e1bcc5f510de3c4ee399cf869d5e41a5e
Anton Volkov <thevadxx@gmail.com>
2022-05-07 13:07:36 +0300

    Easier configure colorscheme

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,15 +1,20 @@
 -- Load nvim color scheme
-local status_ok, color_scheme = pcall(require, 'onedark')
+-- Available: onedark, gruvbox
+theme = 'onedark'
+
+local status_ok, color_scheme = pcall(require, theme) 
 if not status_ok then
   return
 end
 
 -- OneDark styles: dark, darker, cool, deep, warm, warmer, light
-require('onedark').setup {
-  style = 'dark',
-	colors = { fg = '#b2bbcc' } , --default: #a0a8b7
-}
-require('onedark').load()
+if colors == 'onedark' then
+	color_scheme.setup {
+		style = 'dark',
+		colors = { fg = '#b2bbcc' } , --default: #a0a8b7
+	}
+end
+color_scheme.load()
 
 -- statusline color schemes:
 -- import with: require('colors').colorscheme_name
